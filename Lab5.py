9cb7d2d6b43bcb921e8f05039988962beaf45a37
ece-470 <ece-470@users.noreply.github.com>
2017-10-22 20:30:26 +0000

    Add files via upload

--- Lab5.py
+++ Lab5.py
@@ -1,49 +1,95 @@
 import RPi.GPIO as  GPIO
 import time
 
+# These are the pins we will be using.
+# ENA-6, ENB-26, IN1-12, IN2-13, IN3-20, IN4-21
+# CS-5, DataOut-23, Address-24, Clock-25
+ENA = 6
+ENB = 26
+IN1 = 12
+IN2 = 13
+IN3 = 20
+IN4 = 21
+CS = 5
+DataOut = 23
+Address = 24
+Clock = 25
+
+
 numSensors = 5
 
 #Functions to carry out movement of the robot
 # 0 = GPIO.LOW  1 = GPIO.HIGH
 def moveForward():
     print("Moving Forward...")
-    GPIO.output(12,GPIO.HIGH)
-    GPIO.output(13,GPIO.LOW)
-    GPIO.output(20,GPIO.LOW)
-    GPIO.output(21,GPIO.HIGH)
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.HIGH)
 
 def stop():
     print("Stopping...")
-    GPIO.output(12,GPIO.LOW)
-    GPIO.output(13,GPIO.LOW)
-    GPIO.output(20,GPIO.LOW)
-    GPIO.output(21,GPIO.LOW)
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.LOW)
 
 def moveBackward():
     print("Moving Backward...")
-    GPIO.output(12,GPIO.LOW)
-    GPIO.output(13,GPIO.HIGH)
-    GPIO.output(20,GPIO.HIGH)
-    GPIO.output(21,GPIO.LOW)
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.HIGH)
+    GPIO.output(IN3,GPIO.HIGH)
+    GPIO.output(IN4,GPIO.LOW)
 
 def pivotRight():
     print("Right Pivot...")
-    GPIO.output(12,GPIO.HIGH)
-    GPIO.output(13,GPIO.LOW)
-    GPIO.output(20,GPIO.HIGH)
-    GPIO.output(21,GPIO.LOW)
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.HIGH)
+    GPIO.output(IN4,GPIO.LOW)
 
 def pivotLeft():
     print("Left Pivot...")
-    GPIO.output(12,GPIO.LOW)
-    GPIO.output(13,GPIO.HIGH)
-    GPIO.output(20,GPIO.LOW)
-    GPIO.output(21,GPIO.HIGH)
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.HIGH)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.HIGH)
 
 def initRake():
     calibratedMin = [0] * numSensors
     calibratedMax = [1023] * numSensors
     last_value = 0
+
+def readAnalog():
+	value = [0,0,0,0,0,0] #array to hold values of the rake sensor
+	for j in range(0,6):
+		GPIO.output(CS, GPIO.LOW)
+		for i in range(0,4):
+			#sent 4-bit Address
+			if(((j) >> (3 - i)) & 0x01):
+				GPIO.output(Address,GPIO.HIGH)
+			else:
+				GPIO.output(Address,GPIO.LOW)
+			#read MSB 4-bit data
+			value[j] <<= 1
+			if(GPIO.input(DataOut)):
+				value[j] |= 0x01
+			GPIO.output(Clock,GPIO.HIGH)
+			GPIO.output(Clock,GPIO.LOW)
+		for i in range(0,6):
+			#read LSB 8-bit data
+			value[j] <<= 1
+			if(GPIO.input(DataOut)):
+				value[j] |= 0x01
+			GPIO.output(Clock,GPIO.HIGH)
+			GPIO.output(Clock,GPIO.LOW)
+		#no mean ,just delay
+		for i in range(0,6):
+			GPIO.output(Clock,GPIO.HIGH)
+			GPIO.output(Clock,GPIO.LOW)
+#		time.sleep(0.0001)
+		GPIO.output(CS,GPIO.HIGH)
+	return value[1:]
 
 def readLine(white_line = 0):
     #sensor_values = readCalibrated()
@@ -85,28 +131,25 @@ GPIO.setmode(GPIO.BCM)
 # to something other than the default (input)
 GPIO.setwarnings(False)
 
-# These are the pins we will be using.
-# 6 and 26 are ena and enb
-# 12, 13, 20, 21 are IN1 IN2 IN3 IN4
-# CS-5, DataOut-23, Address-24, Clock-25
-chan_list = [5,6,12,13,20,21,24,25,26]
-
 # Sets all the pins stated above as outputs
+chan_list = [ENA,ENB,IN1,IN2,IN3,IN4,Address,CS,Clock]
 GPIO.setup(chan_list,GPIO.OUT)
-GPIO.setup(23,GPIO.IN,GPIO.PUD_UP)
+GPIO.setup(DataOut,GPIO.IN,GPIO.PUD_UP)
 
 # creates objects "p1" and "p2", sets ena and enb to 50 Hz, starts them at 20% duty cycle
-p1 = GPIO.PWM(6,50)
+p1 = GPIO.PWM(ENA,50)
 p1.start(20)
 
-p2 = GPIO.PWM(26,50)
+p2 = GPIO.PWM(ENB,50)
 p2.start(20)
 
 # Stops both the PWM outputs
 p1.stop()
 p2.stop()
 
-print("Number of sensors: " + str(numSensors))
+
+
+
 
 # Cleans up the used resources
 GPIO.cleanup()
